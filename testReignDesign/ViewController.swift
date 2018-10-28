cb31a0a6a8862cf841b266be263b1f5277f9ac40
Cezhar Arevalo <cezhar.arevalo@gmail.com>
2018-10-28 09:53:26 -0300

    Initial Commit

---
+++ testReignDesign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testReignDesign
+//
+//  Created by Cezhar Arévalo on 10/28/18.
+//  Copyright © 2018 TIANDGI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
